19d991970a98a310b86ff5943b04cdcb0386545d
Sunnice <44739306+Sunnice@users.noreply.github.com>
2020-07-31 18:27:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns ./feeds/packages/net/smartdns
 make ./feeds/packages/net/smartdns/install
-./scripts/feeds install -a
 
 rm -rf /package/lean/UnblockNeteaseMusicGo
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/UnblockNeteaseMusicGo package/lean/UnblockNeteaseMusicGo
